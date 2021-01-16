ef65a08a4cbb146ffce3cde68d202e5be26d5ea6
Damien Richcreek <richcreekd@live.com>
2021-01-15 22:04:15 -0800

    Lnkz.io App, First Commit

---
+++ lnkzio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LnkzioConfig(AppConfig):
+    name = 'lnkzio'
---
+++ lnkzio/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class ShortenedURL(models.Model):
+    code = models.CharField(max_length=200)
+    url = models.URLField(blank=True)
